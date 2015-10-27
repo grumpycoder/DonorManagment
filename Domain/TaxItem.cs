0a1d902ed4d8adbf8cdaf4bdaf1cb9d20a34a598
Mark Lawrence <grumpycoder@outlook.com>
2015-10-27 14:08:13 -0500

    update taxitem

--- Domain/TaxItem.cs
+++ Domain/TaxItem.cs
@@ -6,6 +6,7 @@ namespace Domain
     {
         public int Id { get; set; }
         public string ConstituentId { get; set; }
+        public int TaxYear { get; set; }
         public DateTime? DonationDate { get; set; }
         public decimal Amount { get; set; }
 
