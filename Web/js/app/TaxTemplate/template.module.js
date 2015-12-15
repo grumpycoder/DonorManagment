d1b7b3e7f1a171a4280b888bed81b2452e40ba71
Mark Lawrence <grumpycoder@outlook.com>
2015-12-14 18:20:10 -0600

    missed file from commit

---
+++ Web/js/app/TaxTemplate/template.module.js
@@ -0,0 +1,18 @@
+﻿// template.module.js
+
+
+(function() {
+    'use strict';
+    angular.module('app.taxTemplate', []);
+
+
+})();
+
+//(function () {
+//    'use strict';
+//
+////    var app = angular.module('TaxTemplate', []);
+//    window.TaxTemplate = angular.module('TaxTemplate', ['codemwnci.markdown-edit-preview']);
+//
+////    window.TaxTemplate.controller(controllerId, ['$scope', '$http', controllerId]);
+//})();
